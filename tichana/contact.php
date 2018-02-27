718d72bfdfcacb405dc857aa7196a73a6e00966f
Unknown <timat35@gmail.com>
2018-02-27 16:33:01 +0100

    get today evetn from gcal

---
+++ tichana/contact.php
@@ -0,0 +1,149 @@
+
+<!--?php  include('title/title_sacs.php');?-->
+<?php  include('header.php');?>
+
+
+
+<div id="news" class ="container">
+
+	<div class="col-12 text-center">
+	<h2>A la boutique</h2>
+	</div>
+	
+	<div class="row mb-4 justify-content-center">
+		
+		<div class="col-12 col-lg-6  mt-4">
+			<div class="row">
+			
+				<div  class="col-4 text-center align-self-center ">
+					<img   src="img/news/background.png"  alt=""/>
+				</div>
+				<div  class="col-auto align-self-center">
+					<div class="info">
+						<h3>Vernissage</h3>
+						<h5></h5>
+						<p>
+						Vernissage de la boutique, decourverte et apero</br>
+						Le 15 avril apres 18h</br>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+	<div class="row justify-content-center">
+		<div class="col-12 col-lg-6  border-bottom border-warning  mb-4"></div>
+	</div>
+	
+	
+	<div class="col-12 text-center">
+	<h2>En parallele</h2>
+	</div>
+	
+	<div class="row mb-4 mx-4">
+		
+		<div class="col-12 col-lg-6  mt-4">
+			<div class="row">
+			
+				<div  class="col-4 text-center align-self-center ">
+					<img   src="img/news/marche_mens_2016.jpg"  alt="" />
+				</div>
+				<div  class="col-8 align-self-center">
+					<div class="info">
+						<h3>Reza et tet-en-l'air</h3>
+						<h5>Marche de nuit a Mens</h5>
+						<p>
+						7 rue du Lieutenant Chanaron</br>
+						38000 Grenoble</br>
+						Du Mardi au Samedi</br>
+						14h - 19h	
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+
+		<div class="col-12 col-lg-6  mt-4">
+			<div class="row">
+			
+				<div  class="col-4 text-center align-self-center ">
+					<img   src="img/news/festiv_art.png"  alt="" />
+				</div>
+				<div  class="col-8 align-self-center">
+					<div class="info">
+						<h3>Eska</h3>
+						<h5>Nuit de la glisse</h5>
+						<p>
+						blbou lb iblou biboub</br>
+						38000 Grenoble</br>
+						Du Mardi au Samedi</br>
+						14h - 19h	
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-12 col-lg-6  mt-4">
+			<div class="row">
+			
+				<div  class="col-4 text-center align-self-center">
+					<img   src="img/news/marche-noel-2017.jpg"  alt="" />
+				</div>
+				<div  class="col-8 align-self-center">
+					<div class="info">
+						<h3>Le chant de l'email et Tete en l'air</h3>
+						<h5>Marche de nuit a Mens</h5>
+						<p>
+						7 rue du Lieutenant Chanaron</br>
+						38000 Grenoble</br>
+						Du Mardi au Samedi</br>
+						14h - 19h	
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+
+		
+	</div>
+</div>
+
+</div>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel-3.0.6.pack.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src='js/moment.js'></script>
+<script type="text/javascript" src="js/main.js?v=1.5"></script>
+<script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+<script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript" src="js/jquery.event.move.js"></script>
+<script type="text/javascript" src="js/jquery.event.swipe.js"></script>
+<script type="text/javascript" src="js/photoswipe.js"></script>
+<script type="text/javascript" src="js/photoswipe-ui-default.js"></script>
+<script type="text/javascript" src='fullcalendar/fullcalendar.js'></script>
+<script type='text/javascript' src='fullcalendar/gcal.js'></script>
+<script type="text/javascript" src='fullcalendar/locale/fr.js'></script>
+
+<script type="text/javascript">
+
+
+$(document).ready(function() {
+	
+
+});
+
+	
+</script>
+
+
+</body>
+</html>
+
+
