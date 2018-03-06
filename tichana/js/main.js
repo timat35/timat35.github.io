28b86605af811f6ee6df3c3e6cb4df47dd87dd73
Unknown <timat35@gmail.com>
2018-03-06 11:24:55 +0100

    Make news page dinamic
    based on Json

--- tichana/js/main.js
+++ tichana/js/main.js
@@ -139,3 +139,79 @@ function get_items() {
 	
 }
 
+function load_news() {
+	
+	$.getJSON("json/news.json", function(news_data) {
+		
+		var nb_news = Object.keys(news_data).length;
+		console.log(nb_news)
+		for (var i = 1; i <= nb_news; i++) { 
+			console.log(i)
+			
+			var local_news = news_data[i];
+			
+			const temp_img = document.createElement("img");
+			temp_img.setAttribute("src", 'img/news/' + local_news.img);
+			
+			
+			const temp_crea = document.createElement("h3");
+			nb_crea = local_news.creatrice.length;
+			var list_crea= local_news.creatrice[0];
+			
+			for (var j=1; j < nb_crea; j++) {
+				
+				list_crea = list_crea + ((j == (nb_crea-1)) ? " et " : ", ") + local_news.creatrice[j];
+			}
+			temp_crea.innerHTML = list_crea
+			
+			const temp_title = document.createElement("h5");
+			temp_title.innerHTML = local_news.title
+			
+			nb_par = local_news.text.length;
+			
+			const temp_p = document.createElement("p");
+			var temp_desc = local_news.text[0];
+			
+			for (var j=1; j<nb_par; j++) {
+				
+				temp_desc = temp_desc + "<br />" + local_news.text[j];
+			}
+			temp_p.innerHTML = temp_desc;
+			
+			const temp_col_img = document.createElement("div");
+			temp_col_img.setAttribute("class", "col-4 text-center align-self-center");
+			
+			const temp_info = document.createElement("div");
+			temp_info.setAttribute("class", "info");
+			
+			const temp_text = document.createElement("div");
+			temp_text.setAttribute("class", "col-8 align-self-center");
+			
+
+			
+			const temp_row = document.createElement("div");
+			temp_row.setAttribute("class", "row");
+			
+			const temp_base= document.createElement("div");
+			temp_base.setAttribute("class", "col-12 col-lg-6  mt-4");
+			
+			temp_col_img.appendChild(temp_img)
+			
+			temp_info.appendChild(temp_crea)
+			temp_info.appendChild(temp_title)
+			temp_info.appendChild(temp_p)
+			temp_text.appendChild(temp_info)
+			
+			temp_row.appendChild(temp_col_img)
+			temp_row.appendChild(temp_text)
+			temp_base.appendChild(temp_row)
+			document.getElementById("div_marche").appendChild(temp_base)
+			
+		}
+		
+	});
+	
+	
+}
+
+
--- tichana/news.php
+++ tichana/news.php
@@ -23,7 +23,7 @@
 						<h3>Vernissage</h3>
 						<h5></h5>
 						<p>
-						Vernissage de la boutique, decourverte et apero</br>
+						Vernissage de la boutique, decouverte et apero</br>
 						Le 15 avril apres 18h</br>
 						</p>
 					</div>
@@ -42,72 +42,9 @@
 	<h2>En parallele</h2>
 	</div>
 	
-	<div class="row mb-4 mx-4">
-		
-		<div class="col-12 col-lg-6  mt-4">
-			<div class="row">
-			
-				<div  class="col-4 text-center align-self-center ">
-					<img   src="img/news/marche_mens_2016.jpg"  alt="" />
-				</div>
-				<div  class="col-8 align-self-center">
-					<div class="info">
-						<h3>Reza et tet-en-l'air</h3>
-						<h5>Marche de nuit a Mens</h5>
-						<p>
-						7 rue du Lieutenant Chanaron</br>
-						38000 Grenoble</br>
-						Du Mardi au Samedi</br>
-						14h - 19h	
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div id = "div_marche" class="row mb-4 mx-4">
 		
 		
-
-		<div class="col-12 col-lg-6  mt-4">
-			<div class="row">
-			
-				<div  class="col-4 text-center align-self-center ">
-					<img   src="img/news/festiv_art.png"  alt="" />
-				</div>
-				<div  class="col-8 align-self-center">
-					<div class="info">
-						<h3>Eska</h3>
-						<h5>Nuit de la glisse</h5>
-						<p>
-						blbou lb iblou biboub</br>
-						38000 Grenoble</br>
-						Du Mardi au Samedi</br>
-						14h - 19h	
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-12 col-lg-6  mt-4">
-			<div class="row">
-			
-				<div  class="col-4 text-center align-self-center">
-					<img   src="img/news/marche-noel-2017.jpg"  alt="" />
-				</div>
-				<div  class="col-8 align-self-center">
-					<div class="info">
-						<h3>Le chant de l'email et Tete en l'air</h3>
-						<h5>Marche de nuit a Mens</h5>
-						<p>
-						7 rue du Lieutenant Chanaron</br>
-						38000 Grenoble</br>
-						Du Mardi au Samedi</br>
-						14h - 19h	
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
 	
 
 		
@@ -117,25 +54,22 @@
 </div>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!--script type="text/javascript" src="fullcalendar/lib/jquery.min.js"></script-->
 <script type="text/javascript" src="js/jquery.mousewheel-3.0.6.pack.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src='js/moment.js'></script>
 <script type="text/javascript" src="js/main.js?v=1.5"></script>
 <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
 <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
-<script type="text/javascript" src="js/jquery.event.move.js"></script>
-<script type="text/javascript" src="js/jquery.event.swipe.js"></script>
 <script type="text/javascript" src="js/photoswipe.js"></script>
 <script type="text/javascript" src="js/photoswipe-ui-default.js"></script>
-<script type="text/javascript" src='fullcalendar/fullcalendar.js'></script>
-<script type='text/javascript' src='fullcalendar/gcal.js'></script>
-<script type="text/javascript" src='fullcalendar/locale/fr.js'></script>
 
 <script type="text/javascript">
 
 
 $(document).ready(function() {
 	
+	 load_news();
 
 });
 
