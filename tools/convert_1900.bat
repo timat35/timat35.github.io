50721fb845acdc5a6528d23f4bdd142bb1bc0369
Unknown <timat35@gmail.com>
2018-03-25 16:31:35 +0200

    compress photo

---
+++ tools/convert_1900.bat
@@ -0,0 +1,7 @@
+cd C:\local_website\tichana\temp\compresse
+
+for  %%i in (*) do (
+	magick %%i -resize 1425 -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB %%i
+)
+
+cd C:\local_website\tools
--- tools/convert_300.bat
+++ tools/convert_300.bat
@@ -1,7 +1,7 @@
 cd C:\local_website\tichana\temp\compresse
 
 for  %%i in (*) do (
-	magick %%i -resize 2592 -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB thumb-%%i
+	magick %%i -resize 300 -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB thumb-%%i
 )
 
 cd C:\local_website\tools
